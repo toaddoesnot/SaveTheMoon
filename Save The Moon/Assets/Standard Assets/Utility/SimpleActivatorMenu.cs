7ff709dcade08d2d4313ed7bbc9b54557ac13846
toaddoesnot <laiskusn7@gmail.com>
2021-02-26 08:06:33 +0100

    added empty project

---
+++ Save The Moon/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+#pragma warning disable 618
+namespace UnityStandardAssets.Utility
+{
+   // public class SimpleActivatorMenu : MonoBehaviour
+  //  {
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+       // private GUIText camSwitchButton;
+     //   public GameObject[] objects;
+
+
+     //   private int m_CurrentActiveObject;
+
+       // public GUIText CamSwitchButton { get => camSwitchButton; set => camSwitchButton = value; }
+
+      //  private void OnEnable()
+      //  {
+      //      // active object starts from first in array
+       //     m_CurrentActiveObject = 0;
+       //     CamSwitchButton.text = objects[m_CurrentActiveObject].name;
+      //  }
+
+
+       // public void NextCamera()
+      //  {
+      //      int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+
+      //      for (int i = 0; i < objects.Length; i++)
+      //      {
+     //           objects[i].SetActive(i == nextactiveobject);
+     //       }
+
+      //      m_CurrentActiveObject = nextactiveobject;
+      //      CamSwitchButton.text = objects[m_CurrentActiveObject].name;
+     //   }
+   // }
+}
