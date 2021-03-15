f0c0d787e27e6b33a178b7de18640c08828b6349
toaddoesnot <laiskusn7@gmail.com>
2021-03-15 01:42:28 +0100

    Teleports, checkpoints, death

---
+++ Save The Moon/Assets/Save The Moon/Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public HealthController theHealthMan;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theHealthMan = FindObjectOfType<HealthController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            theHealthMan.SetSpawnPoint(transform.position);
+        }
+    }
+
+}
---
+++ Save The Moon/Assets/Save The Moon/Scripts/DeathManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathManager : MonoBehaviour
+{
+    //[SerializeField] 
+    //public Transform player;
+
+    //[SerializeField]
+    //public Transform respawnPoint;
+
+
+    //private void OnTriggerEnter(Collider other)
+   // {
+   //     player.transform.position = respawnPoint.transform.position;
+   // }
+
+   // public void SetSpawnPoint(Transform newPosition)
+   // {
+   //     respawnPoint = newPosition;
+   // }
+}
